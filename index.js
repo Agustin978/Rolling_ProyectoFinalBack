36be0264d1d3a3ad75a63b245ff9e8f7737defe2
Agustin978 <loboagus10@gmail.com>
2023-07-23 20:42:30 -0300

    Importada la coneccion con la base de datos, agregado el config de dotenv y creada una ruta para los usuarios

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import * as dotenv from "dotenv";
+import './src/dataBase/DBConnection';
 
-dotenv.config();
+dotenv.config(); //Para leer variables de entorno
 //Configuro un puerto
 const app = express();
 app.set('port', process.env.PORT || 4000);
@@ -16,6 +17,7 @@ app.listen(app.get('port'), () =>
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(morgan()); //Nos aporta informacion adicional en la terminal
+app.use(morgan('dev')); //Nos aporta informacion adicional en la terminal
 
 //Ingreso de rutas
+app.get('/apiusuarios');
