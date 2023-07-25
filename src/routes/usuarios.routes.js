f3c44aedbcb5ae23f3c3360f69a41b8d58294166
Agustin978 <loboagus10@gmail.com>
2023-07-25 01:39:02 -0300

    Agregada la ruta para obrener todos los usuarios de la base de datos

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { obtenerUsuario } from '../controllers/usuarios.controllers';
+import { obtenerUsuario, obtenerUsuarios } from '../controllers/usuarios.controllers';
 
 const router = Router();
 
-router.route('/usuarios/:id').get(obtenerUsuario);
+router.route('/usuarios/:id').get(obtenerUsuario); //Para la busqueda de un usuario por id
+
+router.route('/usuarios').get(obtenerUsuarios); //Para obtener todos los usuarios almacenados en la bd
 
 export default router;
