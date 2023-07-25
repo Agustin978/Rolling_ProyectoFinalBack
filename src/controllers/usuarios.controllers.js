27a6ee34bc163f174410257d4df2c0a62737642d
Agustin978 <loboagus10@gmail.com>
2023-07-25 02:22:43 -0300

    Agregada la funcion para crear nuevos usuarios, se controla en la misma que el email y el nombre de usuario ingresados no se repita con ningun usuario ya almacenado en la base de datos, y que todos los datos ingresados se encuentren en orden. Tambien se encripta la contraseña del usuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -31,3 +31,60 @@ export const obtenerUsuarios = async (req, res) =>
         });
     }
 }
+
+export const creaUsuario = async (req, res) => 
+{
+    try
+    {
+        //Primero controlo que el usuario no se encuentre ya ingresado
+        const {email, password} = req.body;
+        const {nombreUsuario} = req.body;
+        let usuarioMail = await Usuario.findOne({email: email});
+        let usuarioNombre = await Usuario.findOne({nombreUsuario: nombreUsuario});
+        if(usuarioMail) //Si existe ya un usuario con la direccion de mail ingresada
+        {
+            return res.status(400).json(
+                {
+                    mensaje: 'El mail ingresado ya esta registrado.'
+                }
+            );
+        }else if(usuarioNombre)
+        {
+            return res.status(400).json(
+                {
+                    mensaje: 'El nombre de usuario ingresado ya esta registrado.'
+                }
+            );
+        }else
+        {
+            const errors = validationResult(req.body);
+            if(!errors.isEmpty()) //En caso que hayan errores
+            {
+                return res.status(400).json(
+                    {
+                        error: errors.array()
+                    }
+                );
+            }
+
+            const usuarioNuevo = new Usuario(req.body); //Se crea el nuevo usuario
+            //Debo de encriptar la contraseña del nuevo usuario
+            const salt = bcrypt.genSaltSync(10); //Indico la cantidad de veces que el algoritmo se ejecutara sobre la contraseña
+            usuarioNuevo.password = bcrypt.hashSync(password, salt); //Se encripta la contraseña
+            await usuarioNuevo.save();
+            res.status(201).json(
+                {
+                    mensaje: 'El nuevo usuario fue almacenado exitosamente.',
+                    nombre: usuarioNuevo.nombreUsuario,
+                    uid: usuarioNuevo._id
+                }
+            )
+        }
+    }catch(error)
+    {
+        console.log('A ocurrido un error al intentar comunicarse con la base de datos. Info de error: '+error);
+        res.status(400).json({
+            mensaje: 'Error al intentar ingresar el nuevo usuario en la base de datos.'
+        });
+    }
+}
