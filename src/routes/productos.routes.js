7b12558fa84bf67584ddc66da8b80a259ee2ae6c
Agustin978 <loboagus10@gmail.com>
2023-07-28 17:57:00 -0300

    Agregada la ruta de productosnuevo para referenciar a esa cuando se quera ingresar un nuevo producto; agregada la ruta de productos/:id para referenciar a ella al editar, borrar u obtener un producto usando su id

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,11 +1,56 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { crearProducto, obtenerProducto, obtenerProductos } from '../controllers/productos.controllers';
+import { borrarProducto, crearProducto, editarProducto, obtenerProducto, obtenerProductos } from '../controllers/productos.controllers';
 
 const router = Router();
 
 router.route('/productos')
-      .get(obtenerProductos)
+      .get(obtenerProductos);
+
+router.route('/productos/:id')
+      .get(obtenerProducto)
+      .delete(borrarProducto)
+      .put([
+        check('nombreProducto')
+            .notEmpty()
+            .withMessage('El nombre del producto es un campo obligatorio')
+            .isLength({ min: 2, max: 200 })
+            .withMessage('El nombre del producto debe contener entre 2 y 200 caracteres'),
+        check('imagen')
+            .notEmpty()
+            .withMessage('La imagen es obligatoria')
+            .matches(/^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|webp)$/)
+            .withMessage('La imagen debe ser un enlace y de tipo jpg o gif o png o webp'),
+        check('precioNuevo')
+            .notEmpty()
+            .withMessage('El precio del producto es obligatorio.')
+            .isFloat({ min: 50, max: 10000 })
+            .withMessage('El precio debe ser un número entre 50 y 10000'),
+        check('precioAnterior')
+            .optional({ nullable: true }) 
+            .isFloat({ min: 50, max: 10000 })
+            .withMessage('El precio anterior debe ser un número entre 50 y 10000'),
+        check('descripcion')
+            .notEmpty()
+            .withMessage('La descripción del producto es obligatoria')
+            .isLength({ min: 10, max: 10000 })
+            .withMessage('La descripción del producto debe contener entre 10 y 10000 caracteres'),
+        check('categoria')
+            .notEmpty()
+            .withMessage('La categoría es obligatoria')
+            .isIn([
+                'bebida caliente',
+                'bebida fria',
+                'bebida con alcohol',
+                'entrada',
+                'plato fuerte',
+                'acompaniamientos',
+                'postre',
+            ])
+            .withMessage('Categoría no válida, seleccione una opción válida.')
+        ], editarProducto);
+
+router.route('/productosnuevo')
       .post([
         check('nombreProducto')
             .notEmpty()
@@ -44,6 +89,6 @@ router.route('/productos')
               'postre',
             ])
             .withMessage('Categoría no válida, seleccione una opción válida.')
-      ], crearProducto); 
+      ], crearProducto)
 
 export default router;
