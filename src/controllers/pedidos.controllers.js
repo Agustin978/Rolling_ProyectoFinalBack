557afa0f923378a98a05efdafd28ec1955eb3e97
Martinserrano724 <martinserrano724@gmail.com>
2023-07-28 12:47:32 -0300

    'creacion de editar pedidos y obtener pedidos por id'

--- src/controllers/pedidos.controllers.js
+++ src/controllers/pedidos.controllers.js
@@ -15,7 +15,20 @@ export const obtenerPedidos = async(req, res) =>
         });
     }
 }
-
+export const obtenerPedido = async (req, res) =>
+{
+    try
+    {
+        const pedidos = await Pedido.findById(req.params.id);
+        res.status(200).json(pedidos);
+    }catch(error)
+    {
+        console.log('A ocurrido un error al intentar comunicarse con la base de datos. Info de error: '+error);
+        res.status(400).json({
+            mensaje: 'Error al buscar los pedidos con id: '+req.params.id+' en la base de datos.'
+        });
+    }
+}
 export const creaPedido = async (req, res) =>
 {
     try
@@ -37,6 +50,34 @@ export const creaPedido = async (req, res) =>
         console.log('A ocurrido un error al intentar comunicarse con la base de datos. Info de error: '+error);
         res.status(400).json({
             mensaje: 'Error al ingresar el pedido en la base de datos.'
+        });
+    }
+}
+export const editarPedido = async (req, res) => 
+{
+    try
+    {
+        const errors = validationResult(req);
+        console.log(req.body);
+        if(!errors.isEmpty())
+        {
+            return res.status(400).json(
+                {
+                    error: errors.array()
+                }
+            )
+        }
+        await Pedido.findByIdAndUpdate(req.params.id, req.body);
+        res.status(200).json(
+            {
+                mensaje: 'El pedido fue editado correctamente'
+            }
+        )
+    }catch(error)
+    {
+        console.log('A ocurrido un error al intentar comunicarse con la base de datos. Info de error: '+error);
+        res.status(404).json({
+            mensaje: 'Error, el pedido no pudo ser editado.'
         });
     }
 }
